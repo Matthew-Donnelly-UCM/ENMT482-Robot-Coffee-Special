ea35169f4e94ab53fb42f2f6b0f968c2f9e649c2
Matthew-Donnelly-UCM <matthew.donnelly@ucmotorsport.com>
2026-09-08 15:11:12 +1200

    Extra work done

--- Assignment 2/robodk_stations-master/ClassActions.py
+++ Assignment 2/robodk_stations-master/ClassActions.py
@@ -1,8 +1,10 @@
 from time import sleep
 from robodk.robolink import *
 import tools
+import numpy as np
 RDK = Robolink()
 tls = tools.Tools(RDK)
+import robodk.robomath as rm
 UR5 = RDK.Item("UR5", ITEM_TYPE_ROBOT)
 
 class InitialiseSimulate:
@@ -17,12 +19,33 @@ class InitialiseSimulate:
     robot_program = RDK.Item("Reset_Simulation_R", ITEM_TYPE_PROGRAM)
     robot_program.RunCode()
 class RancilioToMazzerScale:
-    tls.rancilio_tool_attach_r_ati()
+
+    # define a joint angle array for an intermediate point: theta_1, theta_2, ..., theta_6 (from base to tool)
+    angle = np.radians(30)
+    # define an HT for a pose in the world frame, near the cup dispenser. This could be something calculated or copied from the RoboDK GUI
+    T_nearcupdispenser_np = np.array([[ np.cos(angle),     -np.sin(angle),        0,  50 ],
+                                    [  np.sin(angle),       np.cos(angle),        0,  50],
+                                    [     0,              0,            1,              50],
+                                    [  0.000000,     0.000000,     0.000000,     1.000000 ]])
+
+    # convert numpy array into an RDK matrix
+    T_nearcupdispenser = rm.Mat(T_nearcupdispenser_np.tolist())
+
+    # reset the sim
+    robot_program = RDK.Item("Reset_Simulation_R", ITEM_TYPE_PROGRAM)
+    robot_program.RunCode()
+    robot_program.WaitFinished()
+
+    UR5.MoveL(T_nearcupdispenser)
     
 
+    # go back home
+    UR5.MoveJ(RDK.Item("Home_R", ITEM_TYPE_TARGET), True)
+
+
 # example 4x4 matrix 
-#np.array([[ cos(theta),     -sin(theta),     0,         x ],
-#          [  sin(theta),     cos(theta),     0,         y ],
+#np.array([[      0,        0,                0,         x ],
+#          [      0,            0,            0,         y ],
 #          [      0,            0,            1,         z ],
 #          [  0.000000,     0.000000,     0.000000,     1.000000 ]])
 
