8833545f0a94f9907cca5d55c04ef78a9a4cb716
Matthew-Donnelly-UCM <matthew.donnelly@ucmotorsport.com>
2026-09-23 10:24:21 +1200

    working on Part G (final part), Part D left

--- Assignment 2/robodk_stations-master/ActionG.py
+++ Assignment 2/robodk_stations-master/ActionG.py
@@ -19,10 +19,21 @@ def Rotational_matrix_x(theta):
                [0, np.sin(theta), np.cos(theta), 0],
                [0, 0, 0, 0]])
 
+def Rotational_matrix_y(theta):
+    return np.array([[np.cos(theta), 0, np.sin(theta),0],
+               [0, 1, 0,0],
+               [-np.sin(theta),0, np.cos(theta), 0],
+               [0, 0, 0, 0]])
+
 def Rotational_matrix_z_inverse_z(theta):
     return np.transpose(np.array([[np.cos(theta), -np.sin(theta), 0],
                    [np.sin(theta), np.cos(theta), 0],
                    [0, 0, 1]]))
+
+def Rotational_matrix_y_inverse_y(theta):
+    return np.transpose(np.array([[np.cos(theta), 0, -np.sin(theta)],
+                   [0, 1, 0],
+                   [-np.sin(theta), 0, np.cos(theta)]]))
 
 def Translation_matrix(x,y,z):
     return np.array([[0, 0, 0, x],
@@ -43,111 +54,78 @@ def Inverse_transform(R_T,T):
     print(Trans_inv)
     return(Trans_inv)
 
+def inverse_transform_matrix(R, x, y, z):
+    R_T = np.transpose(R)          # inverse of a rotation matrix is its transpose
+    T = np.array([x, y, z])
+
+    Trans_inv = np.identity(4)
+    Trans_inv[0:3, 0:3] = R_T
+    Trans_inv[0:3, 3] = R_T @ (-T)
+    return Trans_inv
+
 # Now for the actual functions
 
-def HomeToMazzerPickUp():
-    theta = -60*np.pi/180
+def MazzerPickUpTo():
+    theta = 180*np.pi/180
     R = Rotational_matrix_z(theta)
-    T = Translation_matrix(439.4,-277.9, 41.9)
-    URtMS_np = R + T
-    
-    R1 = Rotational_matrix_z(0)
-    T1 = Translation_matrix(-12.1,-19, 14.5)
-    MStMSBB_np = R1 + T1
+    T = Translation_matrix(590.3,-98.7, 86)
+    URtWDT_np = R + T
 
-    R2 = Rotational_matrix_z(0)
-    #controlling x to control the level of retraction
-    T2 = Translation_matrix(-50,0, 32)
-    MSBBtMSRP_np = R2 + T2
+    R1 = Rotational_matrix_y(0)
+    T1 = Translation_matrix(-29.3,0, 0)
+    RTBRRtRTBRRT = R1 + T1
 
-    R3 = Rotational_matrix_z((np.pi/180)*-50)
-    T3 = Translation_matrix(0, 0, 0)
-    TCPtTCPF_np = R3 + T3
+    #inverse matrix 
+    RTBRRTtRTBRR = inverse_transform_matrix(R1, -29.3,0,0)
+
+    theta = 1.7*np.pi/180
+    R2 = Rotational_matrix_y(theta)
+    T2 = Translation_matrix(0,0, 0)
+    RTBRtRTBRR = R2 + T2
 
     #inverse matrix
-    R3_inv = Rotational_matrix_z_inverse_z((np.pi/180)*-50)
-    T3_inv = Translation_matrix_inverse(0,0,0)
-    TCPtTCPF_inv_np = Inverse_transform(R3_inv,T3_inv)
+    RTBRRtRTBR = inverse_transform_matrix(R2, 0,0,0)
 
-    R4 = np.array([[0,0,1,0],
-                   [0,-1,0,0],
-                   [1,0,0,0],
-                   [0,0,0,0]])
+    R3 = Rotational_matrix_z(0)
+    T3 = Translation_matrix(28.7,0, 146.3)
+    RTtRTBR_np = R3 + T3
+    
+    #inverse matrix
+    R3_inv = Rotational_matrix_z_inverse_z(0)
+    T3_inv = Translation_matrix_inverse(28.7,0, 146.3)
+    RTBRtRT = Inverse_transform(R3_inv, T3_inv)
+        
+    R4 = Rotational_matrix_z((np.pi/180)*-50)
     T4 = Translation_matrix(0, 0, 0)
-    TCPFtMSRP = R4 + T4
-
-    #inverse matrix
-    R4_3x3 = np.array([[0,0,1,],
-                   [0,-1,0,],
-                   [1,0,0,]])
-    R4_inv = np.transpose(R4_3x3)
+    TCPtRT_np = R4 + T4
+        
+        #inverse matrix
+    R4_inv = Rotational_matrix_z_inverse_z((np.pi/180)*-50)
     T4_inv = Translation_matrix_inverse(0,0,0)
-    TCPFtMSRP_inv_np = Inverse_transform(R4_inv,T4_inv)
+    RTtTCP = Inverse_transform(R4_inv,T4_inv)
 
-    URtTCP = URtMS_np @  MStMSBB_np @ MSBBtMSRP_np  @ TCPFtMSRP_inv_np @ TCPtTCPF_inv_np
-
-    T_URtTCP = rm.Mat(URtTCP.tolist())
-
-    Detach_program = RDK.Item("Show_Mazzer_Scale_Rancilio_Tool", ITEM_TYPE_PROGRAM)
-    Detach_program.RunCode()
-    Detach_program.WaitFinished()
+    #This is for the basket
 
     
+    R4 = np.array([[0,0,1],
+                   [0,1,0],
+                   [-1,0,0]])
+    
+    WDTtRTBBRT = inverse_transform_matrix(R4, 0, 0, 0)
+
+    URtTCP = URtWDT_np @ WDTtRTBBRT @ RTBRRTtRTBRR @ RTBRRtRTBR @ RTBRtRT @ RTtTCP
+    T_URtTCP = rm.Mat(URtTCP.tolist())
+        
+    # convert numpy array into an RDK matrix
+        
     UR5.MoveJ(RDK.Item("Home_R", ITEM_TYPE_TARGET), True)
-    #after detachment move to this point to reduce chance of collision
-    detach_point = [-9.880000, -103.840000, -130.680000, -125.740000, 1.410000, 136.940000]
-    UR5.MoveJ(detach_point, True)
+    
+    #preparation move using cartesian coordinates
+    prep_coords = [-4.790000, -100.240000, -127.070000, -130.180000, 7.060000, 140.400000]
+    UR5.MoveJ(prep_coords, blocking=True)
+        
     UR5.MoveJ(T_URtTCP, blocking=True)
 
-def CollectMazzerTool():
-    
-    #theta = -2.0934094900519744 #theta calculated
-    theta = -60*np.pi/180
-    R = Rotational_matrix_z(theta)
-    T = Translation_matrix(439.4,-277.9, 41.9)
-    URtMS_np = R + T
-
-    R1 = Rotational_matrix_z(0)
-    T1 = Translation_matrix(-12.1,-19, 14.5)
-    MStMSBB_np = R1 + T1
-
-    R2 = Rotational_matrix_z(0)
-    T2 = Translation_matrix(-32,0, 28.07)
-    RTtRTBB_np = R2 + T2
-
-    #inverse matrix
-    R2_inv = Rotational_matrix_z_inverse_z(0)
-    T2_inv = Translation_matrix_inverse(-32,0, 28.07)
-    RTtRTBB_inv_np = Inverse_transform(R2_inv, T2_inv)
-    
-    R3 = Rotational_matrix_z((np.pi/180)*-50)
-    T3 = Translation_matrix(0, 0, 0)
-    TCPtRT_np = R3 + T3
-    
-    #inverse matrix
-    R3_inv = Rotational_matrix_z_inverse_z((np.pi/180)*-50)
-    T3_inv = Translation_matrix_inverse(0,0,0)
-    TCPtRT_inv_np = Inverse_transform(R3_inv,T3_inv)
-
-    RTBBtMSBB_np = np.array([[0,0,1,0],
-                          [0,-1,0,0],
-                          [1,0,0,0],
-                          [0,0,0,1]])
-
-   #inverse matrix simple
-    RTBBtMSBB_inv_np = RTBBtMSBB_np
-
-    URtTCP = URtMS_np @  MStMSBB_np @ RTBBtMSBB_inv_np  @ RTtRTBB_inv_np @ TCPtRT_inv_np
-
-    T_URtTCP = rm.Mat(URtTCP.tolist())
-
-    # convert numpy array into an RDK matrix
-
-
-    UR5.MoveL(T_URtTCP, blocking=True)
-
-    
-    tls.student_tool_attach()
 # example 4x4 matrix 
 #np.array([[      0,        0,                0,         x ],
 #          [      0,            0,            0,         y ],
